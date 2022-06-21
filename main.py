b560663e58dbef864fa2b0a5e00d27c6d1ba401a
Екатерина <1@1.ru>
2022-06-21 16:48:23 +0700

    Added variable a and b

--- main.py
+++ main.py
@@ -2,8 +2,5 @@
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 print('Hello World!')
 
-
-a= 5
-b = 4
-
-c = 8
+a = input('Введите переменную a ')
+b = input('Введите переменную b ')
